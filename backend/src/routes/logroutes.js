8ebe8ede4745d86d2bef495d2d2599357347adef
dineshg04 <dineshkumarg2206@gmail.com>
2026-07-24 00:51:57 +0530

    Audit log System

--- backend/src/routes/logroutes.js
+++ backend/src/routes/logroutes.js
@@ -5,7 +5,7 @@ const logrouter = express.Router();
 const upload = require("../middlewares/upload");
 
 
-const { uploadLogs, getLogs } = require("../controller/logController");
+const { uploadLogs, getLogs } = require("../controller/logcontroller");
 
 
 logrouter.post("/upload", upload.single("file"), uploadLogs);
